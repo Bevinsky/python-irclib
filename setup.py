d98a3b80599823dc1776540658c045739ffd2265
Vin <bevin@jabana.se>
2013-06-17 11:01:52 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="python-irclib",
+    version="0.5.0",
+    url="http://github.com/Bevinsky/python-irclib",
+    description="A heavily modified fork of the original python-irclib by Joel
+    Rosdahl. ",
+    zip_safe=False,
+    platforms="any",
+    packages=[]
+)
